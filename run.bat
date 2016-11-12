bc6f71ffd39087d48282ffd2a77d0698b25b51a3
Brian Burns <bburns.km@gmail.com>
2016-11-11 19:38:19 -0600

    run.bat - remember last program name

--- run.bat
+++ run.bat
@@ -1,6 +1,18 @@
-:: run a python program
-:: was %*
-@python %1.py 
+:: run a python program - remembers last program - no .py needed
 
+@echo off
+
+rem no arguments: run previous program
+rem note: can't use :: comments near the if statements - must use rem
+if "%1" == "" (
+
+  @python %LastPythonProgram%.py 
+ 
+) else (
+
+  set LastPythonProgram=%1
+  @python %1.py
+  
+)
 
 
